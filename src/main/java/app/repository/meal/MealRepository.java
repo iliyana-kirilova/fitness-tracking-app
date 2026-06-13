ef73f505a8fdb48c5931badc2ead54660b1425bc
Iliyana Georgieva Kirilova <iliyana.kirilova.321@gmail.com>
2026-06-13 09:12:39 +0300

    chore: add repository query methods

--- src/main/java/app/repository/meal/MealRepository.java
+++ src/main/java/app/repository/meal/MealRepository.java
@@ -4,8 +4,10 @@ import app.models.entity.meal.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, UUID> {
+    List<Meal> findAllByDailyLogId(UUID dailyLogId);
 }
--- src/main/java/app/repository/userProfile/UserProfileRepository.java
+++ src/main/java/app/repository/userProfile/UserProfileRepository.java
@@ -4,8 +4,10 @@ import app.models.entity.userProfile.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+    Optional<UserProfile> findByUserId(UUID uuid);
 }
